e29c63d9c10921da230b49b3fca10361b64272d4
Frank P <frank.pinder@dss.ca.gov>
2019-07-13 23:00:57 -0700

    update JS File with question array

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,34 +11,165 @@
 // ];
 
 
+var questions = [
+   {
+      question: "What was the first full length CGI movie?",
+      answers: ["A Bug's Life", "Monsters Inc.", "Toy Story", "The Lion King"],
+      correctAnswer: "Toy Story"
+   },
+   {
+      question: "Which of these is NOT a name of one of the Spice Girls?",
+      answers: ["Sporty Spice", "Fred Spice", "Scary Spice", "Posh Spice"],
+      correctAnswer: "Fred Spice"
+   },
+   {
+      question: "Which NBA team won the most titles in the 90s?",
+      answers: ["New York Knicks", "Portland Trailblazers", "Los Angeles Lakers", "Chicago Bulls"],
+      correctAnswer: "Chicago Bulls"
+   },
+   {
+      question: "Which group released the hit song, 'Smells Like Teen Spirit'?",
+      answers: ["Nirvana", "Backstreet Boys", "The Offspring", "No Doubt"],
+      correctAnswer: "Nirvana"
+   },
+   {
+      question: "Which popular Disney movie featured the song, 'Circle of Life'?",
+      answers: ["Aladdin", "Hercules", "Mulan", "The Lion King"],
+      correctAnswer: "The Lion King"
+   },
+   {
+      question: "Finish this line from the Fresh Prince of Bel-Air theme song: 'I whistled for a cab and when it came near, the license plate said...'",
+      answers: ["Dice", "Mirror", "Fresh", "Cab"],
+      correctAnswer: "Fresh"
+   },
+   {
+      question: "What was Doug's best friend's name?",
+      answers: ["Skeeter", "Mark", "Zach", "Cody"],
+      correctAnswer: "Skeeter"
+   },
+   {
+      question: "What was the name of the principal at Bayside High in Saved By The Bell?",
+      answers: ["Mr.Zhou", "Mr.Driggers", "Mr.Belding", "Mr.Page"],
+      correctAnswer: "Mr.Belding"
+   }
+];
 
 var score = 0;
 var questionIndex = 0;
+var correct = 0;
+var incorrect = 0;
+var unAswered = 0;
+var timer; //for set interal time 
+var time = 0;
 
 
+// var questions = [
+//    { q: "In the year 1900 in the U.S. what were the most popular first names given to boy and girl babies?", a: "John and Mary" },
+//    { q: "When did the Liberty Bell get its name?", a: "in the 19th century, when it became a symbol of the abolition of slavery" },
+//    { q: "In the Roy Rogers -Dale Evans Museum, you will find Roy and Dales stuffed horses. Roy's horse was named Trigger, which was Dales horse?", a: "Buttermilk" },
+//    { q: "The Daniel Boon museum at the home where he died can best be described how?", a: " a four-story Georgian-style home in Missouri" }
+// ];
 
-var questions = [
-   { q: "In the year 1900 in the U.S. what were the most popular first names given to boy and girl babies?", a: "John and Mary" },
-   { q: "When did the Liberty Bell get its name?", a: "in the 19th century, when it became a symbol of the abolition of slavery" },
-   { q: "In the Roy Rogers -Dale Evans Museum, you will find Roy and Dales stuffed horses. Roy's horse was named Trigger, which was Dales horse?", a: "Buttermilk" },
-   { q: "The Daniel Boon museum at the home where he died can best be described how?", a: " a four-story Georgian-style home in Missouri" }
-];
-
-var selections = ["William and Elizabeth", "Joseph and Catherine", "John and Mary", "George and Anne", "when it was made, in 1701", "when it rang on July 4, 1776", "in the 19th century, when it became a symbol of the abolition of slavery", "none of the above", "Buttermilk", "Daisy", "Scout", "Tulip", "a log cabin in Kentucky", "a two-story clapboard house in Tennessee", "a four-story Georgian-style home in Missouri", "a three story brick house in Arkansas"
-];
+// var selections = ["William and Elizabeth", "Joseph and Catherine", "John and Mary", "George and Anne", "when it was made, in 1701", "when it rang on July 4, 1776", "in the 19th century, when it became a symbol of the abolition of slavery", "none of the above", "Buttermilk", "Daisy", "Scout", "Tulip", "a log cabin in Kentucky", "a two-story clapboard house in Tennessee", "a four-story Georgian-style home in Missouri", "a three story brick house in Arkansas"
+// ];
 
 // var questionDiv = ('#question');
 
-$(document).ready(function () {
+$(document).ready(function () { 
 
    renderQuestion();
+   startTimer();
 
+   // Function to render questions.
+   function renderQuestion() {
+      var htmlstring = "";
+
+      for (i = 0; i < questions.length; i++) {
+
+         var newDiv = $("<div>")
+         var p = $("<p>")
+
+         p.text(questions[i].question)
+         newDiv.append(p)
+
+         var answers = buildAnswers(questions[i], i)
+         newDiv.append(answers);
+
+         newDiv.addClass('question')
+
+         $("#question").append(newDiv)
+      }
+
+
+   };
+
+   function buildAnswers(question, index) {
+      var answers = $("<div>");
+
+      for (var i = 0; i < question.answers.length; i++) {
+
+         var button = $("<input>")
+         button.attr("type", "radio")
+         button.attr("name", index)
+         button.attr("value", question.answers[i])
+
+         answers.append(button)
+         var span = $(`<span> ${question.answers[i]} </span>`)
+         answers.append(span);
+      }
+
+      return answers;
+   }
+
+   $("#done").on("click", submitButton);
+
+   function submitButton() {
+
+      var inputs = $(".question").find("input:checked")
+      console.log(inputs)
+
+      unAswered = questions.length - inputs.length
+
+      for (var i = 0; i < inputs.length; i++) {
+         //account for unanswered question by name label
+         if ($(inputs[i]).val() === questions[$(inputs[i]).attr("name")].correctAnswer) {
+            console.log(correct)
+            correct++;
+         } else {
+            console.log(incorrect)
+            incorrect++;
+         }
+
+
+
+      }
+      displayResult();
+   }
+
+
+   function displayResult() {
+      $(".container").empty();
+      $(".container").append("<h3>Correct Answers " + correct + "</h3>");
+      $(".container").append("<h3>Incorrect Answers " + incorrect + "</h3>");
+      $(".container").append("<h3>unAswered Answers " + unAswered + "</h3>");
+
+   }
+
+   function startTimer() {
+      timer = setInterval(increment, 1000)
+   }
+
+   function increment() {
+      time++;
+      $("#show-number").text(time)
+      if (time === 10) {
+
+         clearInterval(timer)
+         submitButton();
+
+      }
+
+
+   }
 })
 
-// Function to render questions.
-function renderQuestion() {
-   for (i = 0; i < questions.length; i++) {
-      document.querySelector("#question").innerHTML = questions[i].q;
-  
-   }
-}
